de29d61e45d118e1ff5e85568fc132ad1be3e5d3
Priyankateke <priiyankateke1705@gmail.com>
2020-03-31 14:58:59 -0700

    ADDED: Calculated Employee Wage According To PartTime And FullTime

--- src/main/java/EmployeeWage.java
+++ src/main/java/EmployeeWage.java
@@ -1,6 +1,7 @@
 public class EmployeeWage {
     //Constants
     public static final int IS_FULL_TIME=1;
+    public static final int IS_PART_TIME=2;
     public static final int EMP_RATE_PER_HOURS=20;
 
     //Checking employee is present or absent
@@ -9,11 +10,12 @@ public class EmployeeWage {
         int empHrs=0;
         int empWage=0;
 
-        //getting 0 0r 1 using random function
-        double empCheck=Math.floor(Math.random()*10)%2;
+        double empCheck=Math.floor(Math.random()*10)%3;
 
         if (empCheck == IS_FULL_TIME)
             empHrs=8;
+        else if (empCheck == IS_PART_TIME)
+            empHrs=4;
 
         //calculating daily employee wage
         empWage=empHrs * EMP_RATE_PER_HOURS;
