d0ecce57e8abec44e1ea2b99bab81b5f17ef512f
Priyankateke <priiyankateke1705@gmail.com>
2020-04-01 02:13:21 -0700

    REFACTORED: Functionality To Get Work Hours

--- src/main/java/EmployeeWage.java
+++ src/main/java/EmployeeWage.java
@@ -16,28 +16,36 @@ public class EmployeeWage {
         this.maxHoursPerMonth=maxHoursPerMonth;
     }
 
+    //Method to get the Working Hours Of Employee
+    public int getWorkingHours(int empCheck)
+    {
+        int empHrs;
+        switch (empCheck) {
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            default:
+                empHrs = 0;
+        }
+        return empHrs;
+    }
+
     //Checking employee is present or absent
     public double calculateEmployeeWageForCompany() {
         //Initializing variables
         int dayCount=0,totalEmpHrs=0;
-        int empHrs;
+        int empHours;
 
         //Calculating Wages till a condition of total working hours or days is reached for a month
         while( totalEmpHrs <= maxHoursPerMonth && dayCount < numOfWorkingDays)
         {
             dayCount++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-            switch (empCheck) {
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-            }
-            totalEmpHrs=totalEmpHrs+empHrs;
+            empHours=getWorkingHours(empCheck);
+            totalEmpHrs=totalEmpHrs+empHours;
         }
         //returns employee wages for month
         return totalEmpHrs*empRatePerHour;
