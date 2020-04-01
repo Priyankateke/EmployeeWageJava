37fb41ada7940f652360efdb0d8cca328535f9ce
Priyankateke <priiyankateke1705@gmail.com>
2020-04-01 13:41:48 -0700

    ADDED: Stored Daily Wage With Total Wage Using HashMap

--- src/main/java/EmployeeWage.java
+++ src/main/java/EmployeeWage.java
@@ -1,17 +1,26 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class EmployeeWage {
+class EmployeeWageBuilder {
     //Constants
     public static final int IS_FULL_TIME=1;
     public static final int IS_PART_TIME=2;
 
-    String company;
-    int empRatePerHour;
-    int numOfWorkingDays;
-    int maxHoursPerMonth;
+    //variables
+    public String company;
+    public int empRatePerHour;
+    public int numOfWorkingDays;
+    public int maxHoursPerMonth;
+    public int dayCount=0, totalEmpHrs=0,empHours;
+
+    //For Storing tha daily wage of Employee
+    HashMap<Integer,Integer> empDailyWage=new HashMap<Integer, Integer>();
+
+    //For Storing the Total Wage Of Employee
+    HashMap<String,Integer>totalWages=new HashMap<String, Integer>();
 
     //Constructor
-    public EmployeeWage(String company,int empRatePerHour,int numOfWorkingDays, int maxHoursPerMonth)
+    public EmployeeWageBuilder(String company,int empRatePerHour,int numOfWorkingDays, int maxHoursPerMonth)
     {
         this.company=company;
         this.empRatePerHour=empRatePerHour;
@@ -39,16 +48,13 @@ public class EmployeeWage {
     //Calculating daily wage of employee
     public int calculateDailyWage(int empHours)
     {
-        double wage=empHours*empRatePerHour;
-        return (int) wage;
+        double dailyWage=empHours*empRatePerHour;
+        return (int) dailyWage;
     }
 
-    //Calculating employee wages for different company
-    public int calculateEmployeeWageForCompany() {
-        //Initializing variables
-        int dayCount=0,totalEmpHrs=0;
-        int empHours;
-        int[] empDailyWage=new int[40];
+    //Calculating employee wages and total wages for different company
+    public void calculateEmployeeWageForCompany() {
+        System.out.println("Employee Details of "+company);
 
         //Calculating Wages till a condition of total working hours or days is reached for a month
         while( totalEmpHrs <= maxHoursPerMonth && dayCount < numOfWorkingDays)
@@ -57,30 +63,48 @@ public class EmployeeWage {
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             empHours=getWorkingHours(empCheck);
             totalEmpHrs=totalEmpHrs+empHours;
-            int wage=calculateDailyWage(empHours);
-
-            //Storing daily wages
-            empDailyWage[dayCount]=wage;
+            int dailyWage=calculateDailyWage(empHours);
+            empDailyWage.put(dayCount,dailyWage);
         }
 
-        //returns employee wages for month
-        return totalEmpHrs*empRatePerHour;
+        //calculating employee wages for month
+        int monthSalary=totalEmpHrs*empRatePerHour;
+        totalWages.put(company,monthSalary);
+
+        printDailyWage();
+        companyTotalWage(company);
+    }
+
+    //Displaying daily employee wage
+    public void printDailyWage() {
+        System.out.println("Employee's Daily Wage Of : "+company);
+        for (int i:empDailyWage.keySet()) {
+            System.out.println("Day "+i+" : "+empDailyWage.get(i));
+        }
+    }
+
+    //Displaying Total wage of company
+    public void companyTotalWage(String company) {
+        if(totalWages.get(company)!=null)
+            System.out.println("Total wage of "+company+" "+totalWages.get(company));
+    }
+}
+
+public class EmployeeWage {
+
+    ArrayList<EmployeeWageBuilder> empDetails=new ArrayList<EmployeeWageBuilder>();
+
+    public void empWageDetails(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth) {
+        empDetails.add(new EmployeeWageBuilder(company,empRatePerHour,numOfWorkingDays,maxHoursPerMonth));
+        EmployeeWageBuilder employeeWageBuilder=empDetails.get(empDetails.size()-1);
+        employeeWageBuilder.calculateEmployeeWageForCompany();
     }
 
     public static void main(String[] args) {
         //Displaying Welcome Message
         System.out.println("Welcome To Employee Wage Computation");
-
-        //ArrayList for different companies
-        ArrayList<EmployeeWage> list=new ArrayList<EmployeeWage>();
-
-        EmployeeWage dMart=new EmployeeWage("DMart",30,20,100);
-        EmployeeWage reliance=new EmployeeWage("Reliance",40,30,150);
-
-        list.add(dMart);
-        list.add(reliance);
-
-       System.out.println("Total Employee Wage For Company : " + dMart.company + " is : " + dMart.calculateEmployeeWageForCompany());
-       System.out.println("Total Employee Wage For Company : " + reliance.company + " is : " + reliance.calculateEmployeeWageForCompany());
+        EmployeeWage employeeWage=new EmployeeWage();
+        employeeWage.empWageDetails("DMart",30,20,100);
+        employeeWage.empWageDetails("Reliance",40,30,150);
     }
 }
