1054630659bbb4815ee1c463ee36a63d7179e87e
Priyankateke <priiyankateke1705@gmail.com>
2020-03-31 21:45:59 -0700

    ADDED: Calculated Employee Wages For Month

--- src/main/java/EmployeeWage.java
+++ src/main/java/EmployeeWage.java
@@ -3,32 +3,39 @@ public class EmployeeWage {
     public static final int IS_FULL_TIME=1;
     public static final int IS_PART_TIME=2;
     public static final int EMP_RATE_PER_HOURS=20;
+    public static final int NUM_OF_WORKING_DAYS=20;
 
     //Checking employee is present or absent
-    public static void calculateDailyEmployeeWage() {
+    public static void calculateWagesForMonth() {
         //Initializing variables
         int empHrs=0;
         int empWage=0;
+        int dayCount;
+        double perDaySalary, totalSalary=0;
 
-        int empCheck=(int) Math.floor(Math.random() * 10 ) % 3;
-        switch (empCheck) {
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
-            default:
-                empHrs=0;
+        for( dayCount=1; dayCount<=NUM_OF_WORKING_DAYS;dayCount++ )
+        {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+
+            //calculating employee wages for month
+            perDaySalary = empHrs * EMP_RATE_PER_HOURS;
+            totalSalary=totalSalary+perDaySalary;
         }
-
-        //calculating daily employee wage
-        empWage=empHrs * EMP_RATE_PER_HOURS;
-        System.out.println("Daily Employee wage : " + empWage);
+        System.out.println("Monthly Employee wages : " + totalSalary);
     }
     public static void main(String[] args) {
         //Displaying Welcome Message
         System.out.println("Welcome To Employee Wage Computation");
-        calculateDailyEmployeeWage();
+        calculateWagesForMonth();
     }
 }
